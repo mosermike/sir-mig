[FIX] indx is in the wrong shape when results are read

src/profile_stk.py
@@ -331,7 +331,6 @@ class Profile:
 		# Read all the files
 		for i in range(len(task['x'])):
 			file = f"{os.path.join(path,task['folders'][i])}/{filename}"
-			print(file)
 			ll, I, Q, U, V = read_profile_sir(file)
 			if( (i == 0) and (self.wave == 0).all()):
 				self.wave = ll
@@ -341,7 +340,7 @@ class Profile:
 			self.stku[x, y, :] = U
 			self.stkv[x, y, :] = V
 			
-
+		self.indx = np.zeros(self.wave.shape)
 		self.load = True
 
 		return self