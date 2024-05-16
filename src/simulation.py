06082f6eb3611ac3ab70e992903d363b221dcc89
Mike Moser <moser@leibniz-kis.de>
2024-05-16 16:33:39 +0200

    [DOC] Update for 1 Node

--- src/simulation.py
+++ src/simulation.py
@@ -5,7 +5,7 @@ Simulation
 
 This module provides function to create Stokes Profiles by starting with the models. A typical procedure is the following:
 
- 1. Create Models with 2 or 3 Nodes
+ 1. Create Models with 1, 2 or 3 Nodes
  2. Perform Synthesis of the Models
  3. Add Noise to the synthesised Stokes Profiles
 
@@ -107,78 +107,6 @@ def add_noise(conf: dict, verbose: bool = True) -> None:
 	return
 
 
-def create_temperature(tau, B = 0):
-	r"""
-	Creates a random temperature in the parameter space
-
-	Parameters
-	----------
-	tau : numpy array
-		Array with the $\log\tau$ of the model
-	B : float, optional
-		Value of the magnetic field in the deepest layer. Default: 0
-
-	Returns
-	-------
-	out : numpy array
-		Interpolated numy array with a random temperature atmosphere
-	"""
-	#############################################################################################
-	# The two models hsra and cool11 (Collados M., Martínez Pillet V., Ruiz Cobo B., 		  	#
-	# Del Toro Iniesta J.C., & Vázquez M. 1994 A&A 291 622 (Umbral model for a big spot)) are	#
-	# considered. The minimum is the cool11 model, and then I add a factor of the HSRA model.	#
-	# The structure of this factor is the following:											#
-	# The factors are chosen because of the following thoughts/points:							#
-	# - The temperature starts at the model cool11.												#
-	# - I create a random factor between 0 and 1.												#
-	# - 0 = cool11 Model																		#
-	# - 1 = HSRA Model																			#
-	# - I implemented some dependency on the magnetic field: If the magnetic field is strong, 	#
-	#   the range for the factor is smaller														#
-	#############################################################################################
-	# Values from HSRA and cool11
-	log_taus = np.array([1.4, 1.3, 1.2, 1.1, 1., 0.9, 0.8, 0.7, 0.6, 0.5, 0.4,
-					0.3, 0.2, 0.1, 0., -0.1, -0.2, -0.3, -0.4, -0.5, -0.6, -0.7,
-					-0.8, -0.9, -1., -1.1, -1.2, -1.3, -1.4, -1.5, -1.6, -1.7, -1.8,
-					-1.9, -2., -2.1, -2.2, -2.3, -2.4, -2.5, -2.6, -2.7, -2.8, -2.9,
-					-3., -3.1, -3.2, -3.3, -3.4, -3.5, -3.6, -3.7, -3.8, -3.9, -4.])
-	HSRA_T = np.array([9560., 9390., 9220., 9050., 8880., 8710., 8520., 8290.,
-						8030., 7750., 7440., 7140.9, 6860., 6610., 6390., 6200.,
-						6035., 5890., 5765., 5650., 5540., 5430., 5330., 5240.,
-						5160., 5080., 5010., 4950., 4895., 4840., 4790., 4750.,
-						4720., 4690., 4660., 4630., 4600., 4575., 4550., 4525.,
-						4490., 4460., 4430., 4405., 4380., 4355., 4330., 4305.,
-						4280., 4250., 4225., 4205., 4190., 4175., 4170.])
-	cool11_T = np.array([6780.3, 6536., 6291.9, 6048.5, 5806.5, 5569.5, 5340.7, 5117.3,
-							4902.9, 4700.4, 4513.9, 4342.3, 4188.8, 4053.4, 3940.5, 3854.,
-							3785., 3726.8, 3676.7, 3633.6, 3597.9, 3564.7, 3534.9, 3511.6,
-							3498., 3489.4, 3482.2, 3475.6, 3468.9, 3461.6, 3453.6, 3445.2,
-							3436.4, 3427., 3417.1, 3406.5, 3395.3, 3383.4, 3370.8, 3357.9,
-							3345.1, 3332.4, 3319.2, 3305.5, 3291.1, 3276., 3260.1, 3243.5,
-							3225.9, 3207.5, 3188.5, 3170.5, 3155.7, 3142.8, 3129.7]
-						)
-	HSRA_T -= cool11_T  # Make it relative to cool11 so that with a fac of 1, I get the HSRA model
-	# Factor for adding HSRA depending on the magnetic field
-	if B > 5000:
-		factor = np.random.uniform(0.0, 0.6)
-	elif B > 4000:
-		factor = np.random.uniform(0.0, 0.7)
-	elif B > 3000:
-		factor = np.random.uniform(0.0, 0.8)
-	elif B > 2000:
-		factor = np.random.uniform(0.0, 0.9)
-	else:
-		factor = np.random.uniform(0.6, 1.1)
-	# Little perturbation for cool11 model
-	cool11_T = cool11_T * np.random.uniform(0.95, 1.05)
-	# Add the values
-	Ts = cool11_T + factor * HSRA_T
-	# Add (only in creating models) additional perturbation in a resulting rotation around log tau -1
-	factor = np.random.uniform(0.9, 1.1)
-	Ts[Ts > -1] = Ts[Ts > -1] * factor
-	Ts[Ts <= -1] = Ts[Ts <= -1] / factor
-
-	return np.interp(tau, np.flip(log_taus), np.flip(Ts))
 """
 *****************************************************************************
 *								CREATE MODELS								*
@@ -763,6 +691,79 @@ def create_models(conf: dict) -> None:
 		print(f"[ERROR] Number of nodes {model_nodes} not implemented!")
 
 	return
+
+def create_temperature(tau, B = 0):
+	r"""
+	Creates a random temperature in the parameter space.
+
+	Parameters
+	----------
+	tau : numpy array
+		Array with the $\log\tau$ of the model
+	B : float, optional
+		Value of the magnetic field in the deepest layer. Default: 0
+
+	Returns
+	-------
+	out : numpy array
+		Interpolated numy array with a random temperature atmosphere
+	"""
+	#############################################################################################
+	# The two models hsra and cool11 (Collados M., Martínez Pillet V., Ruiz Cobo B., 		  	#
+	# Del Toro Iniesta J.C., & Vázquez M. 1994 A&A 291 622 (Umbral model for a big spot)) are	#
+	# considered. The minimum is the cool11 model, and then I add a factor of the HSRA model.	#
+	# The structure of this factor is the following:											#
+	# The factors are chosen because of the following thoughts/points:							#
+	# - The temperature starts at the model cool11.												#
+	# - I create a random factor between 0 and 1.												#
+	# - 0 = cool11 Model																		#
+	# - 1 = HSRA Model																			#
+	# - I implemented some dependency on the magnetic field: If the magnetic field is strong, 	#
+	#   the range for the factor is smaller														#
+	#############################################################################################
+	# Values from HSRA and cool11
+	log_taus = np.array([1.4, 1.3, 1.2, 1.1, 1., 0.9, 0.8, 0.7, 0.6, 0.5, 0.4,
+					0.3, 0.2, 0.1, 0., -0.1, -0.2, -0.3, -0.4, -0.5, -0.6, -0.7,
+					-0.8, -0.9, -1., -1.1, -1.2, -1.3, -1.4, -1.5, -1.6, -1.7, -1.8,
+					-1.9, -2., -2.1, -2.2, -2.3, -2.4, -2.5, -2.6, -2.7, -2.8, -2.9,
+					-3., -3.1, -3.2, -3.3, -3.4, -3.5, -3.6, -3.7, -3.8, -3.9, -4.])
+	HSRA_T = np.array([9560., 9390., 9220., 9050., 8880., 8710., 8520., 8290.,
+						8030., 7750., 7440., 7140.9, 6860., 6610., 6390., 6200.,
+						6035., 5890., 5765., 5650., 5540., 5430., 5330., 5240.,
+						5160., 5080., 5010., 4950., 4895., 4840., 4790., 4750.,
+						4720., 4690., 4660., 4630., 4600., 4575., 4550., 4525.,
+						4490., 4460., 4430., 4405., 4380., 4355., 4330., 4305.,
+						4280., 4250., 4225., 4205., 4190., 4175., 4170.])
+	cool11_T = np.array([6780.3, 6536., 6291.9, 6048.5, 5806.5, 5569.5, 5340.7, 5117.3,
+							4902.9, 4700.4, 4513.9, 4342.3, 4188.8, 4053.4, 3940.5, 3854.,
+							3785., 3726.8, 3676.7, 3633.6, 3597.9, 3564.7, 3534.9, 3511.6,
+							3498., 3489.4, 3482.2, 3475.6, 3468.9, 3461.6, 3453.6, 3445.2,
+							3436.4, 3427., 3417.1, 3406.5, 3395.3, 3383.4, 3370.8, 3357.9,
+							3345.1, 3332.4, 3319.2, 3305.5, 3291.1, 3276., 3260.1, 3243.5,
+							3225.9, 3207.5, 3188.5, 3170.5, 3155.7, 3142.8, 3129.7]
+						)
+	HSRA_T -= cool11_T  # Make it relative to cool11 so that with a fac of 1, I get the HSRA model
+	# Factor for adding HSRA depending on the magnetic field
+	if B > 5000:
+		factor = np.random.uniform(0.0, 0.6)
+	elif B > 4000:
+		factor = np.random.uniform(0.0, 0.7)
+	elif B > 3000:
+		factor = np.random.uniform(0.0, 0.8)
+	elif B > 2000:
+		factor = np.random.uniform(0.0, 0.9)
+	else:
+		factor = np.random.uniform(0.6, 1.1)
+	# Little perturbation for cool11 model
+	cool11_T = cool11_T * np.random.uniform(0.95, 1.05)
+	# Add the values
+	Ts = cool11_T + factor * HSRA_T
+	# Add (only in creating models) additional perturbation in a resulting rotation around log tau -1
+	factor = np.random.uniform(0.9, 1.1)
+	Ts[Ts > -1] = Ts[Ts > -1] * factor
+	Ts[Ts <= -1] = Ts[Ts <= -1] / factor
+
+	return np.interp(tau, np.flip(log_taus), np.flip(Ts))
 
 def synthesis(conf, comm, rank, size, MPI):
 	"""
