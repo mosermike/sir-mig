a4c89ae703bf5fc5bbd2c93361f5063f581e2e42
Mike Moser <moser@leibniz-kis.de>
2024-06-04 17:19:03 +0200

    [UPD] smaller change

--- src/profile_stk.py
+++ src/profile_stk.py
@@ -217,10 +217,10 @@ class Profile:
 			List with the ranges in pixel in x and y direction
 
 		"""
-		if((Map[1]-Map[0]+1) >= self.stki.shape[0]):
+		if((Map[1]-Map[0]+1) > self.stki.shape[0]):
 			print(f"[cut_to_map] The selected map region is too big! ({Map[1]-Map[0]+1} vs. {self.stki.shape[0]})")
 			return self
-		if((Map[3]-Map[2]+1) >= self.stki.shape[1]):
+		if((Map[3]-Map[2]+1) > self.stki.shape[1]):
 			print(f"[cut_to_map] The selected map region is too big! ({Map[3]-Map[2]+1} vs. {self.stki.shape[1]})")
 			return self
 		
